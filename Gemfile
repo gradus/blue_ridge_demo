8371472db1a841133d0fd3f584f94ee87111a8f7
Kris Windham <klop@klop.local>
2010-07-29 19:35:08 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta4'
+gem "haml"
+gem "rails3-generators"
+gem 'mysql', '2.8.1'
+gem 'barista', '>= 0.5.0'
+gem 'jasmine', '>=0.11.1.0'
+gem 'evergreen', :require => 'evergreen/rails'
+
+# Bundle gems for certain environments:
+group :test do
+  gem "rspec"
+  gem 'rspec-rails', '>= 2.0.0.beta.19'
+  gem 'capybara'
+end
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.0.0.beta.19"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '54d34d177dc0cc6647cf756548abfd94a8f28718d5183676eeedcc4a2831a325cf13eb189c9c3630103c7fdda7f3636b8d90934b62a2327a2fbf8d434ced0886'
